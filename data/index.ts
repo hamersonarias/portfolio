e50e56ddd4e5dc207f60676ead55cac450d7f0b7
hamersonarias <garzonariashamersonjohan@gmail.com>
2024-08-22 05:21:47 +0900

    update portfolio-veta

---
+++ data/index.ts
@@ -0,0 +1,242 @@
+export const navItems = [
+    
+    { name: "About", link: "#about" },
+    { name: "Skills", link: "#skills"},
+    { name: "Projects", link: "#projects" },
+    { name: "Experience", link: "#experience" },
+    { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+    {
+        id: 1,
+        title: "I focus on working closely with clients, promoting transparent communication",
+        description: "",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        imgClassName: "w-full h-full",
+        titleClassName: "justify-end",
+        img: "/b1.jpg",
+        spareImg: "",
+    },
+    {
+        id: 2,
+        title: "I'm very flexible with time zone communications",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "/3.jpg",
+        spareImg: "",
+    },
+    {
+        id: 3,
+        title: "My goal stack",
+        description: "I continually work on improving",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center",
+        img: "/2.jpg",
+        spareImg: "",
+    },
+    {
+        id: 4,
+        title: "Technology enthusiast passionate about development.",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "/10.jpg",
+        spareImg: "/b5.svg",
+    },
+    {
+        id: 5,
+        title: "Are you interested in starting a project with me?",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+        img: "22.jpg",
+        spareImg: "/grid.svg",
+    },
+];
+
+export const projects = [
+    {
+        id: 1,
+        title: "UPDF AI Online",
+        des: "The Most Powerful AI Assistant to Chat with PDF and Ask Anything Online",
+        img: "/project1.PNG",
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+        link: "https://updf.ai/",
+    },
+    {
+        id: 2,
+        title: "DocsBot AI",
+        des: "Get instant answers for you, your customers, or your team with AI powered chatbots trained with your content and documentation.",
+        img: "/project2.PNG",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+        link: "https://docsbot.ai/",
+    },
+    {
+        id: 3,
+        title: "Bharat Bot",
+        des: "Your Everyday Chatbot Partner",
+        img: "/project3.PNG",
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+        link: "https://bharatbot.ai/",
+    },
+    {
+        id: 4,
+        title: "AI Article Summarizer",
+        des: "A web application that uses AI to summarize articles and text from different blogs.",
+        img: "/19.jpg",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+        link: "https://ai-summarizer-sumz.netlify.app/",
+    },
+];
+
+// export const testimonials = [
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+//     {
+//         quote:
+//             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//         name: "Michael Johnson",
+//         title: "Director of AlphaStream Technologies",
+//     },
+// ];
+
+export const companies = [
+    {
+        id: 1,
+        name: "cloudinary",
+        img: "/cloud.svg",
+        nameImg: "/cloudName.svg",
+    },
+    {
+        id: 2,
+        name: "appwrite",
+        img: "/app.svg",
+        nameImg: "/appName.svg",
+    },
+    {
+        id: 3,
+        name: "HOSTINGER",
+        img: "/host.svg",
+        nameImg: "/hostName.svg",
+    },
+    {
+        id: 4,
+        name: "stream",
+        img: "/s.svg",
+        nameImg: "/streamName.svg",
+    },
+    {
+        id: 5,
+        name: "docker.",
+        img: "/dock.svg",
+        nameImg: "/dockerName.svg",
+    },
+];
+
+export const workExperience = [
+    {
+        id: 1,
+        title: "Full-Stack Web Developer",
+        desc: "Build full-stack Web applications, from smart contracts to front-end interfaces using Solidity & React.js",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+    {
+        id: 2,
+        title: "AI expert engineer",
+        desc: "Helped develop numerous AI chatbots for children's education and Healthcare.",
+        className: "md:col-span-2", // change to md:col-span-2
+        thumbnail: "/exp2.svg",
+    }
+];
+
+export const mySkills = [
+    {
+        id: 1,
+        title: "Gen AI",
+        desc: "",
+        className: "md:col-span-6",
+        thumbnail: "/exp1.svg",
+    },
+    {
+        id: 2,
+        title: "Chatbot",
+        desc: "",
+        className: "md:col-span-6", // change to md:col-span-2
+        thumbnail: "/exp2.svg",
+    },
+    {
+        id: 3,
+        title: "GPT-4",
+        desc: "",
+        className: "md:col-span-6",
+        thumbnail: "/gpt.png",
+    },
+    {
+        id: 4,
+        title: "RAG",
+        desc: "",
+        className: "md:col-span-6", // change to md:col-span-2
+        thumbnail: "/exp4.svg",
+    },
+    {
+        id: 5,
+        title: "LLM",
+        desc: "",
+        className: "md:col-span-6",
+        thumbnail: "/cloud.svg",
+    },
+    {
+        id: 6,
+        title: "Langchain",
+        desc: "",
+        className: "md:col-span-6", // change to md:col-span-2
+        thumbnail: "/c.svg",
+    }
+];
+
+export const socialMedia = [
+    {
+        id: 1,
+        img: "/git.svg",
+        link: "https://github.com/nirban256"
+    },
+    {
+        id: 2,
+        img: "/twit.svg",
+        link: "https://x.com/NirbanChakrab10"
+    },
+    {
+        id: 3,
+        img: "/link.svg",
+        link: "https://www.linkedin.com/in/nirban-chakraborty/"
+    },
+];
